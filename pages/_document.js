97d48aefe581007599b6be4ae48c0a575133ae6a
Gabriel Ramos <gabriel.cvramos@gmail.com>
2022-10-27 02:31:11 -0300

    add static front page

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html lang="en-us" className="dark">
+        <Html lang="en-us" className="dark bg-black">
             <Head />
             <body>
                 <Main />
